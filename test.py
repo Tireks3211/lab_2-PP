8194fb5340c4a9adcf608a916845855832700060
Tireks <iprudnikov1@gmail.com>
2026-05-18 11:00:29 +0300

    redact

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 print("world")
-=======
-print("Hello")
->>>>>>> develop
 def display_string(text: str) -> None:
     #The function takes a text string and prints it to the console using print()
     """Prints the given string."""
